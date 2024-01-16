ae65a108c5e6f50529944ac952aef1dca07ed87f
Peter Kompasz <kompaszbence@gmail.com>
2024-01-16 22:51:17 +0200

    web: init

---
+++ web/src/client/Tracking.js
@@ -0,0 +1,14 @@
+
+// TODO Track package
+// TODO Report damaged/mishandled package (specifically go through weight history to check if something is missing)
+// TODO Proof of delivery
+// TODO Consignment
+const Tracking = () => {
+  return (
+    <div>
+      asd
+    </div>
+  )
+}
+
+export default Tracking;
