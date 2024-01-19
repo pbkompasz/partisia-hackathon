578776c501f13752651f62e5265abc0884c1f5e0
Peter Kompasz <kompaszbence@gmail.com>
2024-01-19 21:04:54 +0200

    web: Error404 page

---
+++ web/src/pages/common/Error404.jsx
@@ -0,0 +1,13 @@
+import Button from "@mui/material/Button";
+import CommonLayout from "../../components/layout/CommonLayout";
+
+const Error404 = () => {
+  return (
+    <CommonLayout>
+      <h1>Error</h1>
+      <Button href="/">Go back</Button>
+    </CommonLayout>
+  );
+};
+
+export default Error404;
