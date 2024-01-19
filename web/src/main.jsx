b29e3483456f24b41133367aa6eada41ddf76c58
Peter Kompasz <kompaszbence@gmail.com>
2024-01-19 21:06:40 +0200

    web: new routes

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -10,9 +10,12 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import App from './App.jsx'
-import Tracking from './client/Tracking.jsx';
-import './index.css'
+import App from './pages/common/App.jsx'
+import Tracking from './pages/common/Tracking.jsx';
+import Register from './pages/common/Register.jsx';
+import Error404 from './pages/common/Error404.jsx';
+import Invite from './pages/common/Invite.jsx';
+import './index.scss'
 
 const router = createBrowserRouter([
   {
@@ -23,6 +26,18 @@ const router = createBrowserRouter([
     path: '/track',
     element: <Tracking />
   },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/invite',
+    element: <Invite />,
+  },
+  {
+    path: '*',
+    element: <Error404 />,
+  }
 ])
 
 
