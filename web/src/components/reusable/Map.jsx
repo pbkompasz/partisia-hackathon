a4fb31c8e6105f458ca90436a56cec7ca6ba211b
Peter Kompasz <kompaszbence@gmail.com>
2024-01-21 21:48:52 +0200

    web: Basic map component

---
+++ web/src/components/reusable/Map.jsx
@@ -0,0 +1,29 @@
+import { Marker, Popup } from "react-leaflet";
+import { MapContainer } from "react-leaflet/MapContainer";
+import { TileLayer } from "react-leaflet/TileLayer";
+import { useMap } from "react-leaflet/hooks";
+
+// TODO
+// Display markers, areas and roads
+// Add possibility to mark an area, only area and points
+const Map = ({height="400px", width="400px"}) => {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      style={{ height, width, }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker> */}
+    </MapContainer>
+  );
+};
+
+export default Map;
