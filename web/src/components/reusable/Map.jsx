88ba46fd0f154e6d4d09714839e26f20547a28a9
Peter Kompasz <kompaszbence@gmail.com>
2024-01-23 19:44:17 +0200

    web: Map display roads

--- web/src/components/reusable/Map.jsx
+++ web/src/components/reusable/Map.jsx
@@ -4,9 +4,6 @@ import { TileLayer } from "react-leaflet/TileLayer";
 import { useMapEvents } from "react-leaflet/hooks";
 import { Polygon, Circle, Polyline, Rectangle } from "react-leaflet";
 
-// TODO
-// Display roads
-
 // This is probably overkill
 // Add possibility to mark an area
 
@@ -30,22 +27,57 @@ const Vector = ({ vector }) => {
   return <></>;
 };
 
+const Route = ({ coordinates }) => {
+  let coordA = coordinates[0];
+  const polygons = [];
+  for (let i = 1; i < coordinates.length; i++) {
+    const coordB = coordinates[i];
+    polygons.push(
+      <Vector
+        vector={{
+          type: "polygon",
+          corners: [coordA, coordB],
+        }}
+      ></Vector>
+    );
+    coordA = coordB;
+  }
+  console.log(polygons);
+  return polygons;
+};
+
 const Map = ({
   height = "400px",
   width = "400px",
   markers = [],
   temporaryMarkers = [],
   vectors = [
-    {
-      id: 1,
-      corners: [
-        [51.49, -0.08],
-        [51.5, -0.06],
-      ],
-      color: "red",
-      type: "rectangle",
-    },
+    // {
+    //   id: 1,
+    //   corners: [
+    //     [51.49, -0.08],
+    //     [51.5, -0.06],
+    //   ],
+    //   color: "red",
+    //   type: "rectangle",
+    // },
   ],
+  routes = [
+    // {
+    //   id: 1,
+    //   coordinates: [
+    //     [51.56764, 0.018659],
+    //     [51.568065, 0.018478],
+    //     [51.568748, 0.018176],
+    //     [51.569449, 0.017866],
+    //     [51.570003, 0.01762],
+    //     [51.570025, 0.017624],
+    //     [51.570049, 0.017637],
+    //   ],
+    // },
+  ],
+  center = [51.505, -0.09],
+  zoom = 13,
   onClick,
 }) => {
   const LocationFinderDummy = () => {
@@ -56,10 +88,9 @@ const Map = ({
     });
     return null;
   };
-  console.log(temporaryMarkers)
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height, width }}>
+    <MapContainer center={center} zoom={zoom} style={{ height, width }}>
       <LocationFinderDummy />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -77,6 +108,9 @@ const Map = ({
       ))}
       {vectors.map((vector) => (
         <Vector key={vector.id} vector={vector}></Vector>
+      ))}
+      {routes.map((route) => (
+        <Route key={route.id} coordinates={route.coordinates}></Route>
       ))}
     </MapContainer>
   );
