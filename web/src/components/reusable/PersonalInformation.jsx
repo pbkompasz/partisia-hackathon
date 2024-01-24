8df6dd4fcd0836d9f1fc4db35035a1bb196f6d43
Peter Kompasz <kompaszbence@gmail.com>
2024-01-24 22:28:09 +0200

    web: PersonalInformation setAccount on change

--- web/src/components/reusable/PersonalInformation.jsx
+++ web/src/components/reusable/PersonalInformation.jsx
@@ -1,17 +1,38 @@
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 
-const PersonalInformation = () => {
+const PersonalInformation = ({ account, setAccount }) => {
+  const onChange = (key, value) => {
+    setAccount((prev) => {
+      prev[`${key}`] = value;
+      return prev;
+    })
+  }
+
   return (
     <>
       <Grid item xs={6}>
-        <TextField required id="first-name" label="First name" />
+        <TextField
+          value={account.firstName}
+          onChange={(e) => onChange("firstName", e.target.value)}
+          required
+          id="first-name"
+          label="First name"
+        />
       </Grid>
       <Grid item xs={6}>
-        <TextField required id="last-name" label="Last name" />
+        <TextField
+          value={account.lastName}
+          onChange={(e) => onChange("lastName", e.target.value)}
+          required
+          id="last-name"
+          label="Last name"
+        />
       </Grid>
       <Grid item xs={12}>
         <TextField
+          value={account.email}
+          onChange={(e) => onChange("email", e.target.value)}
           style={{ width: "100%" }}
           required
           id="email"
