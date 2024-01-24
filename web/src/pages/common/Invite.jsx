ca71a8f25fe819c3ed635ec2801d9c45902054d3
Peter Kompasz <kompaszbence@gmail.com>
2024-01-24 22:25:07 +0200

    web: Refactor invite page * Minor style changes * Personal information component * Backdrop while verifying

--- web/src/pages/common/Invite.jsx
+++ web/src/pages/common/Invite.jsx
@@ -5,99 +5,95 @@ import FormControl from "@mui/material/FormControl";
 import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Switch from "@mui/material/Switch";
-import DialogTitle from "@mui/material/DialogTitle";
-import Dialog from "@mui/material/Dialog";
+import { useState, } from "react";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 
 import CommonLayout from "../../components/layout/CommonLayout";
-import { useState } from "react";
-
-function TokenDialog(props) {
-  const { open, onClose } = props;
-
-  return (
-    <Dialog onClose={() => onClose('random-token-close')} open={open}>
-      <DialogTitle>Set backup account</DialogTitle>
-      <Typography>Your generated token</Typography>
-      <FormControlLabel
-        control={<Switch defaultChecked />}
-        label="Store your private token in the browser"
-      />
-      <Typography>
-        ! This is your unique token. Do not share it with anyone. If you lose it
-        you will not be able to access your token unless the administrator of
-        your organization grants you access.
-      </Typography>
-      <Button onClick={() => onClose('random-token-button')}>Close</Button>
-    </Dialog>
-  );
-}
+import PersonalInformation from "../../components/reusable/PersonalInformation";
+import chain from "../../chain/authenticate";
+import TokenDialog from "../../components/reusable/TokenDialog";
 
 const Invite = (props) => {
   const [showTokenModal, setShowTokenModal] = useState(false);
-  const [token, setToken] = useState("");
+  const [account, setAccount] = useState({});
+  const [verifing, setVerifing] = useState(false);
+  const [verified, setVerified] = useState(false);
 
   const handleModalClose = (generatedToken) => {
+    setVerifing(true);
     setShowTokenModal(false);
-    setToken(generatedToken);
-  }
+    chain.validateToken(generatedToken);
+    setVerifing(false);
+    setVerified(true);
+    setAccount((prev) => {
+      prev.token = generatedToken;
+      return prev;
+    });
+  };
 
   return (
     <CommonLayout>
-      <h1>Invite</h1>
-      <Typography>
-        {props.name} invited you to join {props.organization} as a {props.role}
-      </Typography>
-      <Box component="form" className="form" noValidate autoComplete="off">
-        <div className="form__section">
-          <Typography sx={{ fontSize: 24 }}>
-            Enter your information below
-          </Typography>
-          <FormControl fullWidth sx={{ m: 1 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={6}>
-                <TextField required id="first-name" label="First name" />
-              </Grid>
-              <Grid item xs={6}>
-                <TextField required id="last-name" label="Last name" />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  style={{ width: "100%" }}
-                  required
-                  id="email"
-                  label="Email address"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <Grid container alignItems="center">
-                  <Grid item xs={8}>
-                    <TextField
-                      style={{ width: "100%" }}
-                      required
-                      id="token"
-                      label="Partisia token"
-                      value={token}
-                    />
-                  </Grid>
-                  <Grid item xs={4} container justifyContent="center">
-                    <Button onClick={() => setShowTokenModal(true)}>
-                      I don&apos;t have a token
-                    </Button>
-                    <TokenDialog
-                      open={showTokenModal}
-                      onClose={handleModalClose}
-                    />
+      <Box sx={{ maxWidth: "80%", padding: "2rem" }}>
+        <h1>Invite</h1>
+        <Typography>
+          {props.name} invited you to join {props.organization} as a{" "}
+          {props.role}
+        </Typography>
+        <Box component="form" className="form" noValidate autoComplete="off">
+          <div className="form__section">
+            <Typography sx={{ fontSize: 24 }}>
+              Enter your information below
+            </Typography>
+            <FormControl fullWidth sx={{ m: 1 }}>
+              <Grid container spacing={2}>
+                <PersonalInformation account={account} setAccount={setAccount}></PersonalInformation>
+                <Grid item xs={12}>
+                  <Grid container alignItems="center">
+                    <Grid item xs={8}>
+                      <Stack direction="row" alignItems="center" gap={2}>
+                        <TextField
+                          style={{ width: "100%" }}
+                          required
+                          id="token"
+                          label="Partisia token"
+                          value={account.token}
+                        />
+                        {verified && (
+                          <CheckCircleOutlineIcon color="success"></CheckCircleOutlineIcon>
+                        )}
+                      </Stack>
+                    </Grid>
+                    <Grid item xs={4} container justifyContent="center">
+                      <Button onClick={() => setShowTokenModal(true)}>
+                        I don&apos;t have a token
+                      </Button>
+                      <TokenDialog
+                        open={showTokenModal}
+                        onClose={handleModalClose}
+                      />
+                      {verifing && (
+                        <Backdrop
+                          sx={{
+                            color: "#fff",
+                            zIndex: (theme) => theme.zIndex.drawer + 1,
+                          }}
+                          open={open}
+                        >
+                          <CircularProgress color="inherit" />
+                        </Backdrop>
+                      )}
+                    </Grid>
                   </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-            <Stack direction="row-reverse" style={{ marginTop: "2rem" }}>
-              <Button size="large">Submit</Button>
-            </Stack>
-          </FormControl>
-        </div>
+              <Stack direction="row-reverse" style={{ marginTop: "2rem" }}>
+                <Button size="large">Submit</Button>
+              </Stack>
+            </FormControl>
+          </div>
+        </Box>
       </Box>
     </CommonLayout>
   );
