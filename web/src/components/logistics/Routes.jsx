3cfc55ce2ecb9530bc89142531b32aff6677fe06
Peter Kompasz <kompaszbence@gmail.com>
2024-02-11 22:54:19 +0200

    web: routes component

--- web/src/components/logistics/Routes.jsx
+++ web/src/components/logistics/Routes.jsx
@@ -1,6 +1,21 @@
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
 
 const Routes = () => {
-  return <div>asd</div>;
+  return (
+    <div style={{ padding: "2rem" }}>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" color="inherit" href="/">
+          *
+        </Link>
+        <Link underline="hover" color="inherit" href="/dashboard/routes">
+          Routes
+        </Link>
+      </Breadcrumbs>
+      <h1>Your routes</h1>
+      <h1>Bid</h1>
+    </div>
+  );
 };
 
 export default Routes;
