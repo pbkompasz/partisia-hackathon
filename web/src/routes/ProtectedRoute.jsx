be063b5a0c401c1148e52c36461ecd4f93c82c1b
Peter Kompasz <kompaszbence@gmail.com>
2024-02-11 22:50:58 +0200

    web: authentication

--- web/src/routes/ProtectedRoute.jsx
+++ web/src/routes/ProtectedRoute.jsx
@@ -1,12 +1,33 @@
-import { Navigate } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import { updateAccountChain, updateAccountHost } from "../state/account";
+import { useEffect } from "react";
+import { getAccount } from "../api/chain";
+import { getAccount as getAccountHost } from "../api/host/account";
 
+const ProtectedRoute = ({ redirectTo, children }) => {
+  const dispatch = useDispatch();
+  const [cookies] = useCookies(["token"]);
+  const navigate = useNavigate();
 
-const ProtectedRoute = ({ redirectTo, children, user, }) => {
-  if (true) {
-    console.log("Redirecting");
-    return <Navigate to={redirectTo} replace />;
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let resp = await getAccount(cookies.token);
+        dispatch(updateAccountChain(resp));
+        resp = await getAccountHost();
+        dispatch(updateAccountHost(resp));
+      } catch (error) {
+        console.log(error);
+        navigate('/login');
+      }
+    };
 
+    fetchData();
+  }, []);
+
+  // Fetch account data
   return children;
 };
 
