1039c9baf425747381459f625d2a3277ec7ae060
Peter Kompasz <kompaszbence@gmail.com>
2024-02-11 22:51:12 +0200

    web: settings page

--- web/src/pages/protected/Settings.jsx
+++ web/src/pages/protected/Settings.jsx
@@ -1,9 +1,21 @@
 import Layout from "../../components/layout/Layout";
+import Stack from "@mui/material/Stack";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
 
 const Settings = () => {
   return (
     <Layout>
-      Settings
+      <Stack gap={2} style={{ padding: "2rem" }}>
+        <Breadcrumbs aria-label="breadcrumb" style={{ paddingBottom: "1rem" }}>
+          <Link underline="hover" color="inherit" href="/dashboard">
+            *
+          </Link>
+          <Link underline="hover" color="inherit" href="/settings">
+            Settings
+          </Link>
+        </Breadcrumbs>
+      </Stack>
     </Layout>
   );
 };
