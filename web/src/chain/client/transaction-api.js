62695ffd181682d0f01232e814ea0c7b3d186ea9
Peter Kompasz <kompaszbence@gmail.com>
2024-02-13 00:27:33 +0200

    web: client works

--- web/src/chain/client/transaction-api.js
+++ web/src/chain/client/transaction-api.js
@@ -1,99 +1,108 @@
-/*
- * Copyright (C) 2022 - 2023 Partisia Blockchain Foundation
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 import { CLIENT } from "../state";
+import PartisiaSdk from "partisia-sdk";
+import { serializeTransaction } from "../serialize";
+import account from "../../state/account";
 
-/**
- * API for sending transactions to PBC.
- * The API uses a connected user wallet, to sign and send the transaction.
- * If the transaction was successful it calls a provided function to update the contract state in
- * the UI.
- */
-export class TransactionApi {
-  TRANSACTION_TTL = 60_000;
-  DELAY_BETWEEN_RETRIES = 1_000;
-  MAX_TRIES = TransactionApi.TRANSACTION_TTL / this.DELAY_BETWEEN_RETRIES;
-  userWallet;
-  fetchUpdatedState;
+export const TRANSACTION_TTL = 60_000;
+const DELAY_BETWEEN_RETRIES = 1_000;
+const MAX_TRIES = TRANSACTION_TTL / DELAY_BETWEEN_RETRIES;
 
-  constructor(userWallet, fetch) {
-    this.userWallet = userWallet;
-    this.fetchUpdatedState = fetch;
-  }
-
-  sendTransactionAndWait = (
-    address,
-    rpc,
-    gasCost
-  ) => {
-    return this.userWallet
-      .signAndSendTransaction(
-        {
-          rpc,
-          address,
-        },
-        gasCost
-      )
-      .then((putResponse) => {
-        if (putResponse.putSuccessful) {
-          return this.waitForTransaction(putResponse.shard, putResponse.transactionHash)
-            .then(() => {
-              this.fetchUpdatedState();
-              return putResponse.transactionHash;
-            })
-            .catch((reason) => {
-              throw reason;
-            });
-        } else {
-          throw new Error("Transaction could not be sent");
-        }
-      });
-  };
-
-  delay = (millis) => {
-    return new Promise((resolve) => setTimeout(resolve, millis));
-  };
-
-  waitForTransaction = (
+const waitForTransaction = async (
     shard,
     identifier,
     tryCount = 0
   ) => {
-    return CLIENT.getExecutedTransaction(shard, identifier).then((executedTransaction) => {
-      if (executedTransaction == null) {
-        if (tryCount >= TransactionApi.MAX_TRIES) {
-          throw new Error(
-            'Transaction "' + identifier + '" not finalized at shard "' + shard + '"'
-          );
-        } else {
-          return this.delay(TransactionApi.DELAY_BETWEEN_RETRIES).then(() =>
-            this.waitForTransaction(shard, identifier, tryCount + 1)
-          );
-        }
-      } else if (!executedTransaction.executionSucceeded) {
-        throw new Error('Transaction "' + identifier + '" failed at shard "' + shard + '"');
+    const executedTransaction = await CLIENT.getExecutedTransaction(shard, identifier)
+    if (executedTransaction == null) {
+      if (tryCount >= MAX_TRIES) {
+        throw new Error(
+          'Transaction "' + identifier + '" not finalized at shard "' + shard + '"'
+        );
       } else {
-        return Promise.all(
-          executedTransaction.events.map((e) =>
-            this.waitForTransaction(e.destinationShard, e.identifier)
-          )
-        ).then(() => undefined);
+        return this.delay(DELAY_BETWEEN_RETRIES).then(() =>
+          this.waitForTransaction(shard, identifier, tryCount + 1)
+        );
       }
-    });
+    } else if (!executedTransaction.executionSucceeded) {
+      throw new Error('Transaction "' + identifier + '" failed at shard "' + shard + '"');
+    } else {
+      return await Promise.all(
+        executedTransaction.events.map((e) =>
+          this.waitForTransaction(e.destinationShard, e.identifier)
+        )
+      )
+    }
   };
-}
+
+export const sendTransactionAndWait = async (
+  accountAddress,
+    address,
+    rpc,
+    gasCost
+  ) => {
+    console.log(address, rpc, account, gasCost)
+  const putResponse = await signAndSendTransaction(
+    accountAddress,
+    {
+      rpc,
+      address,
+    },
+    gasCost
+  )
+  console.log(putResponse)
+  if (putResponse.putSuccessful) {
+    await waitForTransaction(putResponse.shard, putResponse.transactionHash);
+    return putResponse.transactionHash;
+  } else {
+    throw new Error("Transaction could not be sent");
+  }
+  
+};
+
+const signAndSendTransaction = async (accountAddress, payload, cost = 100000) => {
+  // To send a transaction we need some up-to-date account information, i.e. the
+  // current account nonce.
+  const partisiaSdk = new PartisiaSdk();
+  await partisiaSdk
+      .connect({
+        // eslint-disable-next-line
+        permissions: ["sign"],
+        dappName: "ChainMove",
+        chainId: "Partisia Blockchain Testnet",
+      })
+  const connection = partisiaSdk.connection;
+
+  const accountData = await CLIENT.getAccountData(accountAddress)
+  if (accountData == null) {
+    throw new Error("Account data was null");
+  }
+  // Account data was fetched, build and serialize the transaction
+  // data.
+  const serializedTx = serializeTransaction(
+    {
+      cost: String(cost),
+      nonce: accountData.nonce,
+      validTo: String(new Date().getTime() + TRANSACTION_TTL),
+    },
+    payload
+  );
+  // Ask the MPC wallet to sign and send the transaction.
+  try {
+    const value = await partisiaSdk.signMessage({
+      payload: serializedTx.toString("hex"),
+      payloadType: "hex",
+      dontBroadcast: false,
+    })
+    console.log(value)
+    return {
+      putSuccessful: true,
+      shard: CLIENT.shardForAddress(connection.account.address),
+      transactionHash: value.trxHash,
+    }; 
+  } catch (error) {
+    console.log(error)
+      return {
+        putSuccessful: false,
+      }
+  }
+};
