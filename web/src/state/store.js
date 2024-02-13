8888d0a07577b6057ff25598f3d57ec6b6729fb8
Peter Kompasz <kompaszbence@gmail.com>
2024-02-13 13:29:28 +0200

    web: contract reducers

--- web/src/state/store.js
+++ web/src/state/store.js
@@ -2,11 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authentication";
 import demoReducer from "./demo";
 import accountReducer from "./account";
+import mapContractReducer from "./map-contract";
+import deliveryContractReducer from "./delivery-contract";
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     demo: demoReducer,
     account: accountReducer,
-  }
+    mapContract: mapContractReducer,
+    deliveryContract: deliveryContractReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 })
