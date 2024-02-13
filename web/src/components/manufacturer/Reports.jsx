df19c8a86cbc6e4ee12cf3686485f39fd9636414
Peter Kompasz <kompaszbence@gmail.com>
2024-02-13 13:35:43 +0200

    web: submit report

--- web/src/components/manufacturer/Reports.jsx
+++ web/src/components/manufacturer/Reports.jsx
@@ -12,20 +12,129 @@ import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import Stack from "@mui/material/Stack";
 import DialogTitle from "@mui/material/DialogTitle";
 import Dialog from "@mui/material/Dialog";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import TextField from "@mui/material/TextField";
+import DialogActions from '@mui/material/DialogActions';
 import { useLocation } from "react-router-dom";
 
 import { getReports } from "../../api/host/reports";
 
 function ReportDialog(props) {
-  const { onClose, report, open } = props;
+  const { onClose, report = {}, open } = props;
+
+  useEffect(() => {
+    console.log(report);
+  });
 
   const handleClose = () => {
     onClose();
   };
 
+  const formatDate = (dateString) => {
+    const dateOptions = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-US", dateOptions);
+  };
+
+  const formatLabel = (labelString) => {
+    let temp = labelString.replace("_", " ");
+    const words = temp.split(" ");
+
+    return words
+      .map((word) => {
+        return word[0].toUpperCase() + word.substring(1);
+      })
+      .join(" ");
+  };
+
+  const isDisabled = () => {
+    const today = new Date();
+    return today < new Date(report.start_date) && today > new Date(report.due_date)
+  }
+
   return (
     <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>{report?.name}</DialogTitle>
+      <DialogTitle>
+        <Stack direction="row" justifyContent="space-between">
+          {report?.name}
+          {report?.status === "upcomming" ? (
+            <span
+              style={{
+                borderRadius: "6px",
+                padding: "2px 6px",
+                color: "white",
+                backgroundColor: "#3f50b5",
+              }}
+            >
+              Upcomming
+            </span>
+          ) : report?.status === "completed" ? (
+            <span
+              style={{
+                borderRadius: "6px",
+                padding: "2px 6px",
+                color: "black",
+                backgroundColor: "lightGreen",
+              }}
+            >
+              Completed
+            </span>
+          ) : (
+            <span
+              style={{
+                borderRadius: "6px",
+                padding: "2px 6px",
+                color: "black",
+                backgroundColor: "#b2ebf2",
+              }}
+            >
+              Upcomming
+            </span>
+          )}
+        </Stack>
+      </DialogTitle>
+      <Divider></Divider>
+      <Stack justifyContent="space-between" style={{ width: "45vw", height: "50vh", padding: "1rem" }}>
+        <div>
+          <span style={{ fontWeight: "bold" }}>Report type:</span>{" "}
+          <span> {report?.type}</span>
+          <br />
+          <span style={{ fontWeight: "bold" }}>
+            Submission start date:
+          </span>{" "}
+          <span> {formatDate(report?.start_date)}</span>
+          <br />
+          <span style={{ fontWeight: "bold" }}>Due date:</span>{" "}
+          <span> {formatDate(report?.due_date)}</span>
+          <br />
+          <span style={{ fontWeight: "bold" }}>Contract address:</span>{" "}
+          <span> {report?.address}</span>
+        </div>
+        <Divider></Divider>
+        <Stack gap={2} p={1}>
+          {report?.inputs?.map((input) => (
+            <TextField
+              key={input}
+              id={`outlined-basic-${input}`}
+              label={formatLabel(input)}
+              variant="outlined"
+              style={{ maxWidth: "12rem" }}
+              disabled={isDisabled}
+            />
+          ))}
+        </Stack>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} disabled={isDisabled}>
+            Submit data
+          </Button>
+        </DialogActions>
+      </Stack>
     </Dialog>
   );
 }
@@ -88,6 +197,9 @@ const Reports = () => {
           Reports
         </Link>
       </Breadcrumbs>
+      <Stack direction="row-reverse">
+        <Button onClick={() => setOpenCreate(true)}>Create new report</Button>
+      </Stack>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
